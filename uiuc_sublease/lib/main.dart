77e37d07efe76bad24da742cde72ad669348258a
adrian-szwejk <aszwe2@illinois.edu>
2023-10-24 17:55:02 -0500

    changed screen

--- uiuc_sublease/lib/main.dart
+++ uiuc_sublease/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uiuc_sublease/presentation/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
+      //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -40,7 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
