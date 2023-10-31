234ae1068e8a84dfe07f395575f21aabe9e987f5
John Tran <johntran@wirelessprv-10-195-129-215.near.illinois.edu>
2023-10-31 17:57:08 -0500

    Search Bar added

--- uiuc_sublease/lib/presentation/home_screen.dart
+++ uiuc_sublease/lib/presentation/home_screen.dart
@@ -13,3 +13,186 @@ class _HomeScreenState extends State<HomeScreen> {
     return const Placeholder();
   }
 }
+
+/* This is the code for the search bar that I copied and pasted straight from the internet.
+I don't know how to actually implement it so we can use it, but it is a good starting point
+to get the display showing. */
+void main() => runApp(const SearchBarApp());
+
+class SearchBarApp extends StatefulWidget {
+  const SearchBarApp({super.key});
+
+  @override
+  State<SearchBarApp> createState() => _SearchBarAppState();
+}
+
+class _SearchBarAppState extends State<SearchBarApp> {
+  bool isDark = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = ThemeData(
+        useMaterial3: true,
+        brightness: isDark ? Brightness.dark : Brightness.light);
+
+    return MaterialApp(
+      theme: themeData,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('UIUC Sublease')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SearchAnchor(
+              builder: (BuildContext context, SearchController controller) {
+            return SearchBar(
+              controller: controller,
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0)),
+              onTap: () {
+                controller.openView();
+              },
+              onChanged: (_) {
+                controller.openView();
+              },
+              leading: const Icon(Icons.search),
+              trailing: <Widget>[
+                Tooltip(
+                  message: 'Change brightness mode',
+                  child: IconButton(
+                    isSelected: isDark,
+                    onPressed: () {
+                      setState(() {
+                        isDark = !isDark;
+                      });
+                    },
+                    icon: const Icon(Icons.wb_sunny_outlined),
+                    selectedIcon: const Icon(Icons.brightness_2_outlined),
+                  ),
+                )
+              ],
+            );
+          }, suggestionsBuilder:
+                  (BuildContext context, SearchController controller) {
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  setState(() {
+                    controller.closeView(item);
+                  });
+                },
+              );
+            });
+          }),
+        ),
+      ),
+    );
+  }
+}
+//This is the end of the search bar.
+
+// Beginning of the code for the bottom navigation bar.
+class _DemoPageState extends State<FormPage> {
+  PageController _myPage = PageController(initialPage: 0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 75,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                iconSize: 30.0,
+                padding: EdgeInsets.only(left: 28.0),
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  setState(() {
+                    _myPage.jumpToPage(0);
+                  });
+                },
+              ),
+              IconButton(
+                iconSize: 30.0,
+                padding: EdgeInsets.only(right: 28.0),
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  setState(() {
+                    _myPage.jumpToPage(1);
+                  });
+                },
+              ),
+              IconButton(
+                iconSize: 30.0,
+                padding: EdgeInsets.only(left: 28.0),
+                icon: Icon(Icons.notifications),
+                onPressed: () {
+                  setState(() {
+                    _myPage.jumpToPage(2);
+                  });
+                },
+              ),
+              IconButton(
+                iconSize: 30.0,
+                padding: EdgeInsets.only(right: 28.0),
+                icon: Icon(Icons.list),
+                onPressed: () {
+                  setState(() {
+                    _myPage.jumpToPage(3);
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+      body: PageView(
+        controller: _myPage,
+        onPageChanged: (int) {
+          print('Page Changes to index $int');
+        },
+        children: <Widget>[
+          Center(
+            child: Container(
+              child: Text('Empty Body 0'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('Empty Body 1'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('Empty Body 2'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('Empty Body 3'),
+            ),
+          )
+        ],
+        physics: NeverScrollableScrollPhysics(), // Comment this if you need to use Swipe.
+      ),
+      floatingActionButton: Container(
+        height: 65.0,
+        width: 65.0,
+        child: FittedBox(
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            // elevation: 5.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
